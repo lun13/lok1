787bfd50fa72e5c2275bda08134c7d228f884573
Pavel <astakhovpavel9@gmail.com>
2023-10-24 15:39:46 +0300

    lol

---
+++ задание 2 практика.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int day;
+    cout << "Введите номер дня недели:"; cin >> day;
+    if ((day == 1) || (day == 5))
+        cout << "В этот день 3 пары и Работа." << endl;
+    else if ((day == 2) || (day == 3) || (day == 4))
+        cout << "В этот день 4 пары и компьютерные игры." << endl;
+    else if ((day == 6) || (day == 7))
+        cout << "Отдых и компьютерные игры." << endl;
+
+    return 0;
+}
---
+++ задание 5 практика.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    int Pales = 0;
+    cout << "Введите номер:";
+    cin >> Pales;
+    if (Pales == 1)
+        cout << "Большой палец\n";
+    else if (Pales == 2)
+        cout << "Указательный палец\n";
+    else if (Pales == 3)
+        cout << "Средний палец\n";
+    else if (Pales == 4)
+        cout << "Безымянный палец\n";
+    else if (Pales == 5)
+        cout << "Мизинец\n";
+    else
+        cout << "Нет больше названий у 5 пальцев\n";
+    return 0;
+
+}
+
